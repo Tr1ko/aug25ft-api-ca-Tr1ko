7bb4ae89b1accf47a46f7a2a9c05167b59ea2973
Trym Solheim <Trym20@hotmail.comit config --global user.email trym20@hotmail.com>
2026-04-10 10:26:40 +0200

    feat: add middware

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,6 +1,25 @@
 // Middleware function to determine if the API endpoint request is from an authenticated user
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+
 function isAuth(req, res, next) {
-	next();
+	const authHeader = req.headers.authorization;
+
+	if(!authHeader) {
+		return res.status(401).send('no token');
+	}
+
+	try {
+		const token = authHeader.split(" ")[1];
+		const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+		req.user = decoded;
+
+		next();
+	} catch (err) {
+		return res.status(401).send("invalid token")
+	}
 }
 
 module.exports = isAuth;
