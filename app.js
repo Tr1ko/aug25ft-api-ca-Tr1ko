5ca40d3d945c11d525e9e606f743d9b42f31950a
Trym Solheim <Trym20@hotmail.comit config --global user.email trym20@hotmail.com>
2026-04-10 10:25:57 +0200

    feat: add database sync and swagger options.

--- app.js
+++ app.js
@@ -3,11 +3,44 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
 
+//routes import
+const categoryRouter = require("./routes/category")
+const todosRouter = require("./routes/todos")
 var usersRouter = require('./routes/users');
+
+//swagger options 
+const options = {
+	definition: {
+		openapi: '3.0.0',
+		info: {
+			title: 'Todo API',
+			version: '1.0.0',
+		},
+	},
+	apis: ['./routes/*.js'],
+};
+
+const swaggerSpec = swaggerJsDoc(options);
+
+//db sync
 require('dotenv').config();
 var db = require('./models');
-db.sequelize.sync({ force: true });
+if (process.env.NODE_ENV !== 'test') {
+	db.sequelize.sync().then(async () => {
+		const statuses = ['Not Started', 'Started', 'Completed', 'Deleted'];
+
+		for (const s of statuses) {
+			await db.Status.findOrCreate({
+				where: { status: s },
+			});
+		}
+
+		console.log('Database synced');
+	});
+}
 
 var app = express();
 
@@ -22,6 +55,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/users', usersRouter);
+app.use("/todos", todosRouter);
+app.use("/category", categoryRouter);
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
